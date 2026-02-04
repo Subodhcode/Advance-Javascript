792bb942e70a109c901f0d6e3071764a401a6116
Subodh Thakur <subodht105@gmail.com>
2026-02-04 10:36:40 +0530

    update

--- 17-9-25 day2 import and export/calculator.js
+++ 17-9-25 day2 import and export/calculator.js
@@ -4,7 +4,6 @@
 
 // export default addData
 
-
 // 2
 // let addData=(num1,num2)=>{
 //     return num1+num2
@@ -12,21 +11,20 @@
 
 // export default addData
 
-
 //3 multiple function
 
-function addData(num1,num2){
-     return num1+num2
- }
+function addData(num1, num2) {
+  return num1 + num21;
+}
 
-let mindData=(num1,num2)=>{
-     return num1-num2
- }
+let mindData = (num1, num2) => {
+  return num1 - num2;
+};
 
- let mulData=(num1,num2)=>{
-     return num1*num2
- }
-  let DivData=(num1,num2)=>{
-     return num1*num2
- }
- export{addData,mindData,mulData,DivData}//named export
+let mulData = (num1, num2) => {
+  return num1 * num2;
+};
+let DivData = (num1, num2) => {
+  return num1 * num2;
+};
+export { addData, mindData, mulData, DivData }; //named export
